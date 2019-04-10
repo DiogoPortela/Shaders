28baee361338129c8bcb6b494415e9942874349d
diogo_m <d>
2019-04-10 11:17:11 +0100

    coiso

--- Assets/_TEST_ZONE/Conversion1.cs
+++ Assets/_TEST_ZONE/Conversion1.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Conversion1 : MonoBehaviour
 {
+    private const int heigth = 256, width = 256;
+
     public ComputeShader computeShader;
     [Range(0, 1)]
     public float value;
@@ -12,10 +14,14 @@ public class Conversion1 : MonoBehaviour
     private Material material;
     private int computeIndex;
     private int clearIndex;
-    private void Start() {
+
+    private ComputeBuffer computeBuffer;
+    private List<Vector3> points;
+    private void Start()
+    {
         material = GetComponent<Renderer>().material;
 
-        alphaBlendTexture = new RenderTexture(256, 256, 24);    //1320
+        alphaBlendTexture = new RenderTexture(width, heigth, 24);    //1320
         alphaBlendTexture.enableRandomWrite = true;
         alphaBlendTexture.Create();
 
@@ -25,16 +31,36 @@ public class Conversion1 : MonoBehaviour
         computeShader.SetTexture(computeIndex, "Result", alphaBlendTexture);
         computeShader.SetTexture(clearIndex, "Result", alphaBlendTexture);
 
-        computeShader.Dispatch(clearIndex, 256 / 8, 256 / 8, 1);
+        Restart();
+
+        computeBuffer = new ComputeBuffer(1, sizeof(float) * 3);
+        computeBuffer.SetData(points);
+        computeShader.SetBuffer(computeIndex, "data", computeBuffer);
     }
 
-    private void Update() {
+    private void Restart()
+    {
+        points = new List<Vector3>();
+        points.Add(new Vector3(Random.Range(0, width), Random.Range(0, heigth), 1));
+        computeShader.SetInt("count", 1);
+        
+        computeShader.Dispatch(clearIndex, width / 8, heigth / 8, 1);
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            Vector3 v = points[i];
+            v = new Vector3(v.x, v.y, v.z + Time.deltaTime);
+        }
+
         computeShader.SetFloat("value", value);
         computeShader.SetFloat("deltaTime", Time.deltaTime);
-        computeShader.Dispatch(computeIndex, 256 / 8, 256 / 8, 1);
+        computeShader.Dispatch(computeIndex, width / 8, heigth / 8, 1);
         material.SetTexture("_AlphaText", alphaBlendTexture);
 
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-            computeShader.Dispatch(clearIndex, 256 / 8, 256 / 8, 1);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            Restart();
     }
 }
