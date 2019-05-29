99e0668c383fbf2b5240065efb6f4e30983f93fc
diogo_m <d>
2019-05-29 17:22:55 +0100

    using new stuff

--- Assets/_TEST_ZONE/BLOB/BlobManager.cs
+++ Assets/_TEST_ZONE/BLOB/BlobManager.cs
@@ -16,23 +16,29 @@ public class BlobManager : MonoBehaviour
     public float blobRadius;
     private int firstPass, lastPass;
 
+    private ComputeBuffer cornersBuffer, cellsBuffer;
+    private float[] corners;
+    private int[] cells;
+
+
     void Start()
     {
         allBlobInstances = garbage;
         material = GetComponent<Renderer>().material;
 
-        firstPass =  blobGenerator.FindKernel("CSTextureRender");
-        lastPass =  blobGenerator.FindKernel("CSMarchingSquares");
+        firstPass =  blobGenerator.FindKernel("CSCornerSamples");
+        lastPass =  blobGenerator.FindKernel("CSCellData");
 
-
-        firstRenderTexture = new RenderTexture(32, 32, 0, RenderTextureFormat.R8);
+        firstRenderTexture = new RenderTexture(32, 32, 0, RenderTextureFormat.ARGB64);
         firstRenderTexture.filterMode = FilterMode.Point;
         firstRenderTexture.enableRandomWrite = true;
         firstRenderTexture.Create();
-        blobGenerator.SetTexture(firstPass, "SmallTexture", firstRenderTexture);
+        //blobGenerator.SetTexture(firstPass, "SmallTexture", firstRenderTexture);
         blobGenerator.SetFloat("textureSize", firstRenderTexture.width);
         
         debugMaterial.SetTexture("_MainTex", firstRenderTexture);
+        blobGenerator.SetTexture(firstPass, "SmallTexture", firstRenderTexture);
+        blobGenerator.SetTexture(lastPass, "SmallTexture", firstRenderTexture);
     }
 
     void Update()
@@ -41,13 +47,17 @@ public class BlobManager : MonoBehaviour
         blobGenerator.SetFloat("meshSize", this.transform.localScale.x);
         blobGenerator.SetFloat("blobRadius", blobRadius);
 
-        blobGenerator.SetFloat("arrayCount", allBlobInstances.Count);
-        SendPositionsToGPU2();
+        SendPositionsToGPU();
+        GenerateCorners();
         blobGenerator.Dispatch(firstPass, firstRenderTexture.width / 4, firstRenderTexture.height / 4, 1);
+
+        cornersBuffer.GetData(corners);
+        blobGenerator.SetBuffer(lastPass, "cornersData", cornersBuffer);
+        blobGenerator.Dispatch(lastPass, firstRenderTexture.width / 4, firstRenderTexture.height / 4, 1);
     }
 
-    private void SendPositionsToGPU2(){
-
+    private void SendPositionsToGPU(){
+        blobGenerator.SetFloat("arrayCount", allBlobInstances.Count);
         ComputeBuffer blobInstanceArrayBuffer = new ComputeBuffer(allBlobInstances.Count, sizeof(float) * 2);
         List<Vector2> blobInstanceVectors = new List<Vector2>();
         foreach(Transform t in allBlobInstances){
@@ -56,5 +66,21 @@ public class BlobManager : MonoBehaviour
         blobInstanceArrayBuffer.SetData(blobInstanceVectors);
 
         blobGenerator.SetBuffer(firstPass, "blobInstanceArray", blobInstanceArrayBuffer);
+    }
+    private void GenerateCorners(){
+        int blobImageSize = 32;
+        blobGenerator.SetFloat("arrayData", blobImageSize);
+
+        corners = new float[blobImageSize * blobImageSize];
+        cells = new int[(blobImageSize - 1) * (blobImageSize - 1)];
+
+        cornersBuffer = new ComputeBuffer(corners.Length, sizeof(float));
+        cellsBuffer = new ComputeBuffer(cells.Length, sizeof(int));
+
+        cornersBuffer.SetData(corners);
+        cellsBuffer.SetData(cells);
+
+        blobGenerator.SetBuffer(firstPass, "cornersData", cornersBuffer);
+        blobGenerator.SetBuffer(lastPass, "cellsData", cellsBuffer);
     }
 }
