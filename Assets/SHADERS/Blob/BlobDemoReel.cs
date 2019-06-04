6789cf4e216696d4d7c31cf13a7aab972b8d54f9
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2019-06-04 23:14:16 +0100

    Demo Reel done

--- Assets/SHADERS/Blob/BlobDemoReel.cs
+++ Assets/SHADERS/Blob/BlobDemoReel.cs
@@ -8,20 +8,41 @@ public class BlobDemoReel : MonoBehaviour
     public int createQuanitity;
     public GameObject template;
     public Transform parent;
+    private List<GameObject> spawnedObjs;
 
     // Start is called before the first frame update
     void Start()
     {
         manager = GetComponent<BlobManager>();
 
+        spawnedObjs = new List<GameObject>();
+        Generate();
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)){
+            Generate();
+        }
+    }
+
+    private void Generate(){
+        if(spawnedObjs.Count > 0){
+            foreach (var o in spawnedObjs)
+            {
+                Destroy(o);
+            }
+            spawnedObjs.Clear();
+        }
+
         List<Transform> data = new List<Transform>();
         for(int i = 0; i < createQuanitity; i++){
             var obj = Instantiate(template);
             obj.transform.SetParent(parent);
-            obj.transform.localPosition = new Vector3(Random.Range(-0.5f, 0.5f), 0.1f,  Random.Range(-0.5f, 0.5f));
+            obj.transform.localPosition = new Vector3(Random.Range(-0.9f, 0.9f), 0.02f,  Random.Range(-0.9f, 0.9f));
             obj.transform.localRotation = Quaternion.identity;
             obj.transform.localScale = Vector3.one * 0.02f;
             data.Add(obj.transform);
+            spawnedObjs.Add(obj);
         }
 
         manager.SetObjects(data);
